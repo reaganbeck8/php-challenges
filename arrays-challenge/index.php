a023c962980c7bd7f0ea8802f4b84638311141ee
reaganbeck8 <reaganbeck8@gmail.com>
2018-11-13 14:08:02 +0200

    created working environment for arrays challenge and variables challenge

---
+++ arrays-challenge/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Arrays - Challenge</title>
+</head>
+<body>
+    <?php
+    
+    
+    $food = array('apple', 'avocado', 'nectarine','pumpkin', 'carrot');
+
+    var_dump($food);
+
+    array_shift($food[0]);
+
+    ?>
+</body>
+</html>
---
+++ variables-challenge/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Variables Challenge</title>
+    
+</head>
+<body>
+    <?php
+
+        $name = ' Bob ';
+
+        $age = 35;
+
+        $job = 'plumber';
+
+        $carMake = ' Toyota';
+
+        $carColour = 'RED';
+
+        $happy = true 
+
+        $retirement = '30 years';
+
+        $car = $carColour + $carMake;
+    ?>
+</body>
+</html>
